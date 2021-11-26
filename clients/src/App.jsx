7d979492ac9287aa2f45f361a829be834c5aeb12
Kanyonga Chouella <kanyonga.chouell@gmail.com>
2021-11-26 04:42:34 +0300

    might be the last

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -16,7 +16,7 @@ import { useSelector } from "react-redux";
 
 
 const App = () => {
-  const user = false;
+  const user =  useSelector((state) => state.user.currentUser);
   return (
     <Router>
       <Switch>
